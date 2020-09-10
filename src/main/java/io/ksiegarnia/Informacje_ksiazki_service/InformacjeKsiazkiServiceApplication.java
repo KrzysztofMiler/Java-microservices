52d5b01fb42a45e9d93337e6d95534e54e5419f2
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-10 16:27:58 +0200

    działa z danymi testowymi

---
+++ src/main/java/io/ksiegarnia/Informacje_ksiazki_service/InformacjeKsiazkiServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ksiegarnia.Informacje_ksiazki_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacjeKsiazkiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacjeKsiazkiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ksiegarnia/Informacje_ksiazki_service/InformacjeKsiazkiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ksiegarnia.Informacje_ksiazki_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacjeKsiazkiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
