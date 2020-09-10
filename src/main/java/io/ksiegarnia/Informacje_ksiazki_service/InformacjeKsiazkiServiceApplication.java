80b23e7dafe343353a5f228d916fb151d071d72c
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-10 18:39:18 +0200

    podłączone do eureka server

--- src/main/java/io/ksiegarnia/Informacje_ksiazki_service/InformacjeKsiazkiServiceApplication.java
+++ src/main/java/io/ksiegarnia/Informacje_ksiazki_service/InformacjeKsiazkiServiceApplication.java
@@ -2,8 +2,10 @@ package io.ksiegarnia.Informacje_ksiazki_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InformacjeKsiazkiServiceApplication {
 
 	public static void main(String[] args) {
