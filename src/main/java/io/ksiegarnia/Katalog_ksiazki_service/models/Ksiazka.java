88b1376c03eb6a6b93d0b0f4cbc15695d372b76f
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-10 16:12:51 +0200

    drugi test pobiera dane z InformacjeKsiążka i wyświetla, jest restTemaplate Działa

---
+++ src/main/java/io/ksiegarnia/Katalog_ksiazki_service/models/Ksiazka.java
@@ -0,0 +1,32 @@
+package io.ksiegarnia.Katalog_ksiazki_service.models;
+
+public class Ksiazka {
+    private String ksiazkaId;
+
+    public Ksiazka() {
+    }
+
+    public Ksiazka(String ksiazkaId, String nazwa) {
+        this.ksiazkaId = ksiazkaId;
+        this.nazwa = nazwa;
+    }
+
+    public String getKsiazkaId() {
+        return ksiazkaId;
+    }
+
+    public void setKsiazkaId(String ksiazkaId) {
+        this.ksiazkaId = ksiazkaId;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    private String nazwa;
+
+}
--- src/main/java/io/ksiegarnia/Katalog_ksiazki_service/resources/KatalogKsiazkiResource.java
+++ src/main/java/io/ksiegarnia/Katalog_ksiazki_service/resources/KatalogKsiazkiResource.java
@@ -1,12 +1,18 @@
 package io.ksiegarnia.Katalog_ksiazki_service.resources;
 
 import io.ksiegarnia.Katalog_ksiazki_service.models.KatalogItem;
+import io.ksiegarnia.Katalog_ksiazki_service.models.Ksiazka;
+import io.ksiegarnia.Katalog_ksiazki_service.models.Oceny;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/katalog")
@@ -14,6 +20,22 @@ public class KatalogKsiazkiResource {
 
     @RequestMapping("/{userID}")
     public List<KatalogItem> getCatalog(@PathVariable("userID") String userID){
-        return Collections.singletonList(new KatalogItem("ks","asd",3));//testowe
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        List<Oceny> oceny = Arrays.asList(
+                new Oceny(3,"sdasdasda"),
+            new Oceny(2,"sdasddsadsadasdsadasasda"),
+            new Oceny(5,"sdsda")
+        );
+
+        return oceny.stream().map(ocena -> {
+            Ksiazka ksiazka = restTemplate.getForObject("http://localhost:8081/ksiazka/" + ocena.getKsiazkaID(), Ksiazka.class);
+            return new KatalogItem(ksiazka.getNazwa(),"asd",ocena.getOcena());
+        }).collect(Collectors.toList());
+
+
+        //return Collections.singletonList(new KatalogItem(userID,"asd",3));//testowe
     }
 }
