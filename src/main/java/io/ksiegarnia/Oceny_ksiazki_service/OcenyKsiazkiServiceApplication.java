711d3e57c70d150e9569cb58932a972d46083d92
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-10 16:29:38 +0200

    nowe API będzie pokazywać jakie user zrecenzował książki jak narazie dane na sztywno

---
+++ src/main/java/io/ksiegarnia/Oceny_ksiazki_service/OcenyKsiazkiServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ksiegarnia.Oceny_ksiazki_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcenyKsiazkiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcenyKsiazkiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ksiegarnia/Oceny_ksiazki_service/OcenyKsiazkiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ksiegarnia.Oceny_ksiazki_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcenyKsiazkiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
