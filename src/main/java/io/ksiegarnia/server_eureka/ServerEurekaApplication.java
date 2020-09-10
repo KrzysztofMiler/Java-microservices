34045919a4c7e59e820281c3d51f6fd984bfac95
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-10 17:13:17 +0200

    Działa

---
+++ src/main/java/io/ksiegarnia/server_eureka/ServerEurekaApplication.java
@@ -0,0 +1,15 @@
+package io.ksiegarnia.server_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerEurekaApplication.class, args);
+	}
+
+}
